468e92ae96906da99afe1be52c87cf74df506d7c
Maximilian Stölzle <maximilian@stoelzle.ch>
2019-10-07 17:38:20 +0200

    implemented color detector method

--- packages/colordetector_suscriber/src/suscriber_node.py
+++ packages/colordetector_suscriber/src/suscriber_node.py
@@ -18,38 +18,48 @@ class SuscriberNode(DTROS):
         self.color = 'red'
 
     def callback(self, data):
-        rospy.loginfo("I heard %s", data.data)
-        self.color_detection(data, self.color)
+        rospy.loginfo("I received a message")
+        debug_img = self.color_detection(data, self.color)
+        
 
     def color_detection(self, img, color):
         # converting from BGR to HSV color space
         hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
         if color == 'red':
-            # Range for lower red
-            lower_red = np.array([0,120,70])
-            upper_red = np.array([10,255,255])
-            mask1 = cv2.inRange(hsv, lower_red, upper_red)
-
-            # Range for upper range
-            lower_red = np.array([170,120,70])
-            upper_red = np.array([180,255,255])
-            mask2 = cv2.inRange(hsv,lower_red,upper_red)
-
-
-        # Generating the final mask to detect red color
-        mask = mask1+mask2
-
-        mask1 = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
-        mask1 = cv2.morphologyEx(mask, cv2.MORPH_DILATE, np.ones((3,3),np.uint8))
+            lower = np.array([155,25,0])
+            upper = np.array([179,255,255])
+        elif color == 'blue':
+            lower = np.array([110,50,50])
+            upper = np.array([130,255,255])
+        elif color == 'yellow':
+            lower =  np.array([21, 39, 64])
+            upper =  np.array([40, 255, 255])
+        else:
+            print('unknown color')
+            return
 
 
-        #creating an inverted mask to segment out the cloth from the frame
-        mask2 = cv2.bitwise_not(mask1)
+        mask = cv2.inRange(hsv, lower, upper)
 
+        # The bitwise and of the frame and mask is done so
+        # that only the blue coloured objects are highlighted
+        # and stored in res
+        res = cv2.bitwise_and(img,img, mask= mask)
 
-        #Segmenting the cloth out of the frame using bitwise and with the inverted mask
-        res1 = cv2.bitwise_and(img, img,mask=mask2)
+        # create debug image
+        debug_img = img
+
+        # get object contour
+        im_bw = cv2.cvtColor(res, cv2.COLOR_RGB2GRAY)
+        thresh, img_bw = cv2.threshold(im_bw, 60, 255, cv2.THRESH_BINARY)
+        contours, hierarchy = cv2.findContours(img_bw, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        cv2.drawContours(res, contours, -1, (0,255,0), 3)
+        for i in range(0,len(contours)):
+            x,y,w,h = cv2.boundingRect(contours[i])
+            cv2.rectangle(debug_img, (x,y), (x+w,y+h), (0, 255, 0),3)
+
+        return debug_img
 
 if __name__ == '__main__':
     # create the node
